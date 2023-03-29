973dd35c205e17011608815d0c39c8f04411720d
JoeBosz <joeboz2213@gmail.com>
2023-03-28 21:55:37 -0500

    update proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/": "http://localhost:4000",
+    },
+  },
   resolve: {
     alias: {
       "@": "/src",
