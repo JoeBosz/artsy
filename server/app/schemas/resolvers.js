bd5c18395c0308ea49a62b3e2c4ccab8815a3382
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-04-02 18:30:46 -0500

    Add sign token

--- server/app/schemas/resolvers.js
+++ server/app/schemas/resolvers.js
@@ -1,4 +1,6 @@
+import { AuthenticationError } from "apollo-server-express";
 import { Assignment, Class, Student } from "../models/index.js";
+import { signToken } from "../utils/auth.js";
 
 const resolvers = {
   Query: {
@@ -13,11 +15,34 @@ const resolvers = {
     },
   },
   Mutation: {
+    createStudent: async (parent, { username, email, password }) => {
+      // create a new user
+      const user = await Student.create({ username, email, password });
+      // sign the token
+      const token = signToken(user);
+      // return the token
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      // look up by email
+      const user = await Student.findOne({ email });
+      // if there is no user, return an error
+      if (!user) {
+        throw new AuthenticationError("No user with this email found!");
+      }
+      // if there is a user, check the password
+      const correctPw = await user.isCorrectPassword(password);
+      // if the password is incorrect, return an error
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+      // if the password is correct, sign the token
+      const token = signToken(user);
+      // return the token
+      return { token, user };
+    },
     createClass: async (_, { newClass }) => {
       return await Class.create(newClass);
-    },
-    createStudent: async (_, { newStudent }) => {
-      return await Student.create(newStudent);
     },
     createAssignment: async (_, { newAssignment }) => {
       return await Assignment.create(newAssignment);
