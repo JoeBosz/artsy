3288bab8b79c0c84203e773fff3cc5bfc880daa5
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-04-03 13:38:00 -0500

    Resolve error in apollo server for user login

--- server/app/schemas/resolvers.js
+++ server/app/schemas/resolvers.js
@@ -1,6 +1,4 @@
-import { AuthenticationError } from "apollo-server-express";
 import { Assignment, Class, Student } from "../models/index.js";
-import { signToken } from "../utils/auth.js";
 
 const resolvers = {
   Query: {
@@ -15,31 +13,13 @@ const resolvers = {
     },
   },
   Mutation: {
-    createStudent: async (parent, { username, email, password }) => {
-      // create a new user
-      const user = await Student.create({ username, email, password });
-      // sign the token
-      const token = signToken(user);
-      // return the token
-      return { token, user };
+    async createStudent(parent, { newStudent }) {
+      const token = await Student.create(newStudent);
+      return { token };
     },
-    login: async (parent, { email, password }) => {
-      // look up by email
-      const user = await Student.findOne({ email });
-      // if there is no user, return an error
-      if (!user) {
-        throw new AuthenticationError("No user with this email found!");
-      }
-      // if there is a user, check the password
-      const correctPw = await user.isCorrectPassword(password);
-      // if the password is incorrect, return an error
-      if (!correctPw) {
-        throw new AuthenticationError("Incorrect password!");
-      }
-      // if the password is correct, sign the token
-      const token = signToken(user);
-      // return the token
-      return { token, user };
+    async login(parent, { username, password }) {
+      const token = await Student.findOne({ username });
+      return token;
     },
     createClass: async (_, { newClass }) => {
       return await Class.create(newClass);
