5811cd167e56292528cc77958cfd13e9e2425d51
Tony Dandino <tdandino1005@gmail.com>
2023-04-03 20:32:11 -0500

    Added photos, created navigation page to work

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -1,15 +1,41 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-export default function Navigation() {
+// export default function Navigation() {
+// return (
+// <nav className="bg-gray-700">
+// {/* <ul className="flex justify-center gap-x-8 p-4 text-lg"> */}
+// {/* <li> */}
+// {/* <Link to="/">🏠 Home</Link> */}
+// {/* </li> */}
+// {/* <li> */}
+// {/* <Link to="/about">📖 About</Link> */}
+// {/* </li> */}
+// {/* </ul> */}
+// {/* </nav> */}
+// );
+// }
+
+export default function Navigation({ setPage }) {
   return (
-    <nav className="bg-gray-700">
+    <nav>
       <ul className="flex justify-center gap-x-8 p-4 text-lg">
         <li>
-          <Link to="/">🏠 Home</Link>
+          <a href="#home" onClick={() => setPage("home")}>
+            Home
+          </a>
         </li>
         <li>
-          <Link to="/about">📖 About</Link>
+          <a href="loginForm" onClick={() => setPage("loginForm")}>
+            Login
+          </a>
         </li>
+        <li>
+          <a href="loginRegister" onClick={() => setPage("loginRegister")}>
+            Register
+          </a>
+        </li>
+
+        <img src="./img/Artphoto.jpg" alt="Art" width="100" height="100" />
       </ul>
     </nav>
   );
