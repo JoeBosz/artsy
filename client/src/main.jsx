6ce5e1623a5ca56dad31e5fd8a7b344fb3d1266d
Tony Dandino <tdandino1005@gmail.com>
2023-04-03 22:10:42 -0500

    updated login and register

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import { ApolloClient,ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 import "./index.css";
 
-// TODO: Set up Apollo Client
+const httpLink = createHttpLink({
+  uri: "http:/localhost:4000/",
+});
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  uri:'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
+console.log("CLIENT", client);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+   </React.StrictMode>
 );
